bd0b2026f017bb6ca16254e348bb633c847de79f
Rohit Damkondwar <rdamkondwar@cs.wisc.edu>
2016-10-20 00:45:47 -0500

    Adding segment init, read in loop

--- stat_server.c
+++ stat_server.c
@@ -27,8 +27,61 @@ int main(int argc, char *argv[]) {
   int shmid = create_shared_segment(key);
 
   printf("shmid = %d\n", shmid);
+
+  // sleep(10);
+  char *seg_addr = attach_shared_segment(shmid);
+  printf("attached to segment\n");
+  initialize_segment(seg_addr);
+  printf("init complete\n");
   
+  while (1) {
+    read_segment(seg_addr);
+    sleep(1);
+  }
+  // sleep(10);
   return 0;
+}
+
+void read_segment(char *addr) {
+  // Read first int
+  printf("init = %d\n", *((int *)addr));
+  addr += sizeof(int);
+  // Read 16 ints
+  int i;
+  for (i=0; i < 16; i++) {
+    int val = *((int *)addr);
+    if (val > 0) {
+      printf("child %d = %d\n", i+1, val);
+    }
+    addr += sizeof(int);
+  }
+}
+
+void initialize_segment(char *addr) {
+  // Put 1 as first int
+  int *init_complete = (int *) addr;
+  *init_complete = 1;
+
+  addr+=sizeof(int);
+
+  int i;
+  for (i=0; i < 16; i++) {
+    *((int *)addr) = 0;
+    addr += sizeof(int);
+  }
+
+  // 2 implies init complete
+  *init_complete = 2;
+}
+
+char* attach_shared_segment(int shmid) {
+  char *p = (char *)shmat(shmid, NULL, 0);
+  if ((char *)-1 == p) {
+    perror("shmat: \n");
+    exit(1);
+  }
+  // printf("p = %c\n", *p);
+  return p;
 }
 
 int create_shared_segment(int key) {
--- stat_server.h
+++ stat_server.h
@@ -6,10 +6,14 @@
 #include <unistd.h>
 #include <string.h>
 #include <ctype.h>
+#include <sys/types.h>
+#include <sys/shm.h>
 
 //Util func signatures.
 int parseKey(int *, char*);
 
 //Stat server func signatures.
 int create_shared_segment(int key);
-
+char* attach_shared_segment(int shmid);
+void initialize_segment(char *addr);
+void read_segment(char *addr);
